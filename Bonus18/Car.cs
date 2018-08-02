c8c4625b89e53a7a59cf354e46fd8cd71a479a02
LiegeTaco <ashketchup.pkmn2gmail.com>
2018-08-02 09:26:23 -0400

    First commit

---
+++ Bonus18/Car.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+
+    class Car
+    {
+
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+
+        public string Make
+        {
+
+            set { make = value;  }
+            get { return make; }
+
+        }
+
+        public string Model
+        {
+
+            set { model = value; }
+            get { return model; }
+
+        }
+
+        public int Year
+        {
+
+            set { year = value; }
+            get { return year; }
+
+        }
+
+        public double Price
+        {
+
+            set { price = value; }
+            get { return price; }
+
+        }
+
+        public Car(string make, string model, int year, double price)
+        {
+
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.price = price;
+
+        }
+
+        public Car()
+        {
+
+            make = "Dodge";
+            model = "Challenger";
+            year = 2018;
+            price = 27295.00;
+
+        }
+
+        public override string ToString()
+        {
+
+            return $"{Make, -7} {Model, -10} ({Year}): ${Price, -8}";
+            
+        }
+
+    }
+
+}
---
+++ Bonus18/UsedCar.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+
+    class UsedCar:Car
+    {
+
+        private double mileage;
+
+        public double Mileage
+        {
+
+            set { mileage = value; }
+            get { return mileage; }
+
+        }
+
+        public UsedCar():base()
+        {
+
+            Mileage = 20000;
+            Make = "Honda";
+            Model = "Civic";
+            Year = 2008;
+            Price = 15000;
+
+        }
+
+        public UsedCar(Car c, double mi)
+        {
+
+            Mileage = mi;
+            Make = c.Make;
+            Model = c.Model;
+            Year = c.Year;
+            Price = c.Price - 10000 - (mi / 10);
+
+        }
+
+        public UsedCar(string ma, string mo, int ye, double pr, double mi)
+            :base(ma, mo, ye, pr)
+        {
+            
+            Mileage = mi;
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" ({Mileage} mi)";
+        }
+
+    }
+
+}
